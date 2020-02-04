c17070a4fed46f81486dec2e8dfa5b923c1911e4
NoahNovick <nnovickfaille1@gmail.com>
2020-02-04 18:03:34 -0500

    equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+let eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for(let i = 0; i < array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false;
+      }
+    }
+  return true;
+  }
+  
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
