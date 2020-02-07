156cb508e038001aa24c44118eca37f13b50c346
NoahNovick <nnovickfaille1@gmail.com>
2020-02-07 18:32:48 -0500

    recursive eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -9,9 +9,12 @@ let eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
-
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) || Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   }
@@ -22,23 +25,28 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-  if (
-    Array.isArray(Object.keys(object1)) === true ||
-    Array.isArray(Object.keys(object2)) === true
-  ) {
-    return false;
-  }
-
   for (let key of Object.keys(object1)) {
-    if (!Array.isArray(object1[key])) {
+    if (typeof object1[key] === "object" && !Array.isArray(object1[key])) {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (!Array.isArray(object1[key])) {
       if (object1[key] !== object2[key]) {
         return false;
       }
-    } else {
-      return eqArrays(object1[key], object2[key]);
+    } else if (!eqArrays(object1[key], object2[key])) {
+      return false;
     }
   }
   return true;
 };
 
-eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 });
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }));
+
+//to check if objects are equal:
+//first check if the objects contain the same number of keys: by doing this *** see line: 25 ***
+// secondly, check to see what the values of those keys are by looping through the key values *** see line: 28 ***
+//within the loop, check to see if the types of values are equal(string, number, object, arrays)
+//*** see line: 29 *** to see if the values are nested, and if the nested values are equal)
+// *** see line: 33 *** to check to make sure the value is not an array and then on 34 to see if those values are inequal
+// at the very end, checking to see if the values are arrays, to check to see if theyre equal or not
