e5d5fa0c190032335fe9499aa926f5a21bb2ded2
NoahNovick <nnovickfaille1@gmail.com>
2020-02-10 13:11:43 -0500

    further changes to lotide, adding more tests using require and export

---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+tail(words);
+assertEqual(words.length, 3);
